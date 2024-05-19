e2d23184267164eaea521fe747d09f3518b6eaf5
alexandermentormate <alexander.bozhkov@mentormate.com>
2024-05-19 15:37:09 +0300

    feat: Include netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-
+import adapter from '@sveltejs/adapter-netlify';
 // import adapter from '@sveltejs/adapter-static';
 // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +9,16 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		}),
 
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/svelte_gh_pages' : ''
