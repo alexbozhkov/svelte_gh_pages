d38c9c4df6f91caf7efbe0b34510f903adc18257
alexandermentormate <alexander.bozhkov@mentormate.com>
2024-05-19 14:39:42 +0300

    feat: Integrate maplibre boilerplate example

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
+// import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte-preprocess';
+
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
- 
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
-        adapter: adapter(),
+		adapter: adapter(),
+
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/svelte_gh_pages' : '',
         }
-    }
+	}
 };
 
 export default config;
-
