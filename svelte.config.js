6e70416e44f5322a1a205a8561b8ded344d11d99
alexandermentormate <alexander.bozhkov@mentormate.com>
2024-05-19 14:58:49 +0300

    fix: Revert svelte.config.js optins, because > sveltekit-maplibre-boilerplate@0.0.1 build /home/abozhkov/work/svelte_gh_pages > rimraf build && vite build
    vite v5.2.11 building SSR bundle for production...
    transforming...
    ✓ 99 modules transformed.
    rendering chunks...
    vite v5.2.11 building for production...
    transforming...
    ✓ 71 modules transformed.
    rendering chunks...
    computing gzip size...
    .svelte-kit/output/client/_app/version.json                              0.03 kB │ gzip:   0.05 kB
    .svelte-kit/output/client/.vite/manifest.json                            3.01 kB │ gzip:   0.58 kB
    .svelte-kit/output/client/_app/immutable/assets/2.CRipqT4I.css          65.46 kB │ gzip:   9.23 kB
    .svelte-kit/output/client/_app/immutable/entry/start.tkjHBcE4.js         0.07 kB │ gzip:   0.08 kB
    .svelte-kit/output/client/_app/immutable/chunks/stores.BoqgBBzT.js       0.23 kB │ gzip:   0.17 kB
    .svelte-kit/output/client/_app/immutable/chunks/index.BdjqWB8H.js        0.45 kB │ gzip:   0.32 kB
    .svelte-kit/output/client/_app/immutable/nodes/1.B134oaw-.js             0.84 kB │ gzip:   0.52 kB
    .svelte-kit/output/client/_app/immutable/chunks/scheduler.CuL0dozn.js    2.27 kB │ gzip:   1.06 kB
    .svelte-kit/output/client/_app/immutable/nodes/0.CVCb2S4D.js             3.28 kB │ gzip:   1.30 kB
    .svelte-kit/output/client/_app/immutable/chunks/index.DHM27iWn.js        5.65 kB │ gzip:   2.40 kB
    .svelte-kit/output/client/_app/immutable/entry/app.nevwWSYN.js           6.12 kB │ gzip:   2.49 kB
    .svelte-kit/output/client/_app/immutable/chunks/entry.D77IYR0c.js       27.21 kB │ gzip:  10.74 kB
    .svelte-kit/output/client/_app/immutable/nodes/2.qOkbDSth.js           793.96 kB │ gzip: 216.32 kB
    ✓ built in 2.20s
    .svelte-kit/output/server/.vite/manifest.json                           2.59 kB
    .svelte-kit/output/server/_app/immutable/assets/_page.BVVQIy9i.css     65.37 kB
    .svelte-kit/output/server/internal.js                                   0.31 kB
    .svelte-kit/output/server/entries/pages/_page.ts.js                     0.32 kB
    .svelte-kit/output/server/entries/fallbacks/error.svelte.js             0.47 kB
    .svelte-kit/output/server/chunks/stores.js                              0.78 kB
    .svelte-kit/output/server/chunks/index.js                               1.33 kB
    .svelte-kit/output/server/entries/pages/_layout.svelte.js               1.95 kB
    .svelte-kit/output/server/chunks/ssr.js                                 3.55 kB
    .svelte-kit/output/server/chunks/exports.js                             5.96 kB
    .svelte-kit/output/server/chunks/internal.js                            6.09 kB
    .svelte-kit/output/server/index.js                                    114.10 kB
    .svelte-kit/output/server/entries/pages/_page.svelte.js             1,153.55 kB
    ✓ built in 5.37s
    
    Run npm run preview to preview your production build locally.
    
    > Using @sveltejs/adapter-auto
      Could not detect a supported production environment. See https://kit.svelte.dev/docs/adapters to learn how to configure your app to run on the platform of your choosing
      ✔ done was failing

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-// import adapter from '@sveltejs/adapter-auto';
-// import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter(),
